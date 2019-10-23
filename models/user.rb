920434a439b63a99b243c50e19eaa719dea8eb5b
Jessica Marais <jessica_marais@hotmail.com>
2019-10-23 15:25:26 +0100

    added a signout feature, all tests passing

--- models/user.rb
+++ models/user.rb
@@ -1,12 +1,16 @@
 require 'sinatra/activerecord'
+require 'bcrypt'
 
 class User < ActiveRecord::Base
   has_many :spaces
   has_many :requests
 
+  has_secure_password :recovery_password, validations: false
+
   def self.authenticate(email:, password:)
     existing_user = User.find_by(email: email)
     return nil unless existing_user
+    # Comment out line below as issue still needs to be fixed.
     # return nil unless BCrypt::Password.new(existing_user.password) == password
     { user_id: existing_user.id, user_name: existing_user.username }
   end
