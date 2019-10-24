e3a5f076e4fa509e7d5b0c017e627b11c1d5eb66
JDtheGeek <john.dennehy@gmail.com>
2019-10-24 23:26:39 +0100

    Adds several fixes and adds ability to add dates available per space

--- models/space.rb
+++ models/space.rb
@@ -16,4 +16,19 @@ class Space < ActiveRecord::Base
     this_request.save
   end
 
+  def add_dates(start_date:, end_date:)
+    dates = (start_date...end_date).to_a
+    dates.each do |date|
+      Availability.create(spaces_id: self.id, date_offered: date)
+    end
+  end
+
+  def delete_dates(start_date:, end_date:)
+    dates = (start_date...end_date).to_a
+    dates.each do |date|
+      delete_date = Availability.where(spaces_id: self.id, date_offered: date)
+      Availability.destroy(delete_date[0].id)
+    end
+  end
+
 end
