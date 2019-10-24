82e7a7278398015780af6dcb7c0cff42047e1e27
robertamangiapane <roberta.mangiapane@gmail.com>
2019-10-24 12:15:03 +0100

    adding test to book a place

--- spec/features/view_one_space_details_spec.rb
+++ spec/features/view_one_space_details_spec.rb
@@ -9,10 +9,18 @@ feature 'user can view details for one space' do
   end
 
   scenario 'user can go back to spaces page' do
-    space = Space.create(name: "Space Name", description: "Testing", ppnd: 5000)
+    Space.create(name: "Space Name", description: "Testing", ppnd: 5000)
     visit '/spaces'
     click_link('See more...')
     click_link('Go back...')
     expect(current_path).to eq("/spaces")
+  end
+
+  scenario 'user can book the place' do
+    Space.create(name: "Space Name", description: "Testing", ppnd: 5000)
+    visit '/spaces'
+    click_link('See more...')
+    click_link('Book this space')
+
   end
 end
