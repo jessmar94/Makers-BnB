dd6be45bd112048951b0c66ca0870787b8e1ace5
robertamangiapane <roberta.mangiapane@gmail.com>
2019-10-25 09:53:04 +0100

    implement datepicker

--- spec/features/view_space_spec.rb
+++ spec/features/view_space_spec.rb
@@ -24,24 +24,26 @@ feature 'user can view individual space page' do
 
   end
 
-  scenario "shows the spaces availability" do
-    space = Space.create(name: "Space Name", description: "Testing", ppn: 5000)
-
-    dates = ['2019-10-31','2019-11-02','2019-12-03']
-    dates.each do |date|
-      Availability.create(spaces_id: space.id, date_offered: date)
-    end
-
-    visit "/space/:#{space.id}"
-
-    Availability.create(spaces_id: space.id, date_offered: '2019-10-31')
-    Availability.create(spaces_id: space.id, date_offered: '2019-11-30')
-
-
-    visit "/spaces/#{space.id}"
-
-    save_and_open_page
-    expect(page).to have_content "31 Oct"
-    expect(page).to have_content "30 Nov"
-  end
+  # we no longer need this test with the datepicker
+  
+  # scenario "shows the spaces availability" do
+  #   space = Space.create(name: "Space Name", description: "Testing", ppn: 5000)
+  #
+  #   dates = ['2019-10-31','2019-11-02','2019-12-03']
+  #   dates.each do |date|
+  #     Availability.create(spaces_id: space.id, date_offered: date)
+  #   end
+  #
+  #   visit "/space/:#{space.id}"
+  #
+  #   Availability.create(spaces_id: space.id, date_offered: '2019-10-31')
+  #   Availability.create(spaces_id: space.id, date_offered: '2019-11-30')
+  #
+  #
+  #   visit "/spaces/#{space.id}"
+  #
+  #   save_and_open_page
+  #   expect(page).to have_content "31 Oct"
+  #   expect(page).to have_content "30 Nov"
+  # end
 end
