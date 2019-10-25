3d6747a39e4be1ab62b2fda35d587a8e2b4b9e94
Student <john.dennehy@gmail.com>
2019-10-25 11:19:18 +0100

    Updates Space instance methods to allow addition and deletion of available dates

--- models/space.rb
+++ models/space.rb
@@ -12,23 +12,33 @@ class Space < ActiveRecord::Base
 
   def decline_request(request_id:)
     this_request = Request.find(request_id)
-    this_request.status = "Accepted"
+    this_request.status = "Declined"
     this_request.save
   end
 
-  def add_dates(start_date:, end_date:)
-    dates = (start_date...end_date).to_a
+  def add_dates(start_date:, end_date: nil)
+    return Availability.create(spaces_id: self.id, date_offered: start_date) if end_date.nil?
+
+    dates = (start_date..end_date).to_a
     dates.each do |date|
       Availability.create(spaces_id: self.id, date_offered: date)
     end
   end
 
-  def delete_dates(start_date:, end_date:)
-    dates = (start_date...end_date).to_a
+  def delete_dates(start_date:, end_date: nil)
+    if end_date.nil?
+      dates = [start_date]
+    else
+      dates = (start_date..end_date).to_a
+    end
     dates.each do |date|
       delete_date = Availability.where(spaces_id: self.id, date_offered: date)
       Availability.destroy(delete_date[0].id)
     end
   end
 
+  def edit_description (text:)
+    self.description = text
+    self.save
+  end
 end
