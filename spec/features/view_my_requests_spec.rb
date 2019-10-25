480b95e994412693c9df5cc52d7682a840d86995
robertamangiapane <roberta.mangiapane@gmail.com>
2019-10-25 12:26:41 +0100

    fixed tests failing when user make a request

--- spec/features/view_my_requests_spec.rb
+++ spec/features/view_my_requests_spec.rb
@@ -1,22 +1,17 @@
 feature 'My requests' do
   scenario 'I can see the requests I have made' do
-    # Space.create(name: "Space Name", description: "Testing", ppn: 5000)
+    space = Space.create(name: "Space Name", description: "Testing", ppn: 5000)
     # create a space is needed because of truncate database in test
-    user_signup
-    click_button 'Explore'
-    click_link 'See more...'
+    visit("/spaces/#{space.id}")
     click_button 'Request this space'
-    save_and_open_page
-    # expect(current_path).to eq('/requests')
+    expect(current_path).to eq("/requests")
     expect(page).to have_content 'My Requests'
   end
 
   scenario 'I can see the requests I have received' do
-    # Space.create(name: "Space Name", description: "Testing", ppn: 5000)
+    space = Space.create(name: "Space Name", description: "Testing", ppn: 5000)
     # create a space is needed because of truncate database in test
-    user_signup
-    click_button 'Explore'
-    click_link 'See more...'
+    visit("/spaces/#{space.id}")
     click_button 'Request this space'
     expect(page).to have_content "Requests I've Received"
   end
